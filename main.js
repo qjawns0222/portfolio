919a1263d1ebbb12241169637d669c2538826818
qjawns0222 <33923212+qjawns0222@users.noreply.github.com>
2022-01-02 15:15:00 +0900

    js work animation

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const nav=document.querySelector('#navbar');
 const menu=document.querySelector('.navbar__menu');
 const contact=document.querySelector('.home__contact');
 const arrow=document.querySelector(".up__arrow");
-
+const work=document.querySelectorAll(".project");
+const workbtn=document.querySelector(".work__catergories");
 const navtop=nav.getBoundingClientRect().height;
 //바에 클래스 추가해서 고정되게하는것(기본을 고정으로 하고 기본색깔을 transparent로 통해 투명하게 그리고 내려오면 색깔을 넣어서 만든다)
 const fixnav=()=>{
@@ -44,9 +45,109 @@ const arrvis=()=>{
 
 // 제위로 당기는거
 const uptotop=()=>window.scrollTo(0,0);
+//target 이름으로 프로젝트 숨기고 보이게 하는것 그리고 내려갔다 올라오는 애니매이션+ 다사라졌다가 다시 올라옴
+const proofilter=(event)=>{
+    
+    const filter=event.target.innerText.split(' ')[0];
+    
+    if(filter>=0)
+        return;
+    setTimeout(()=>{
+    setTimeout(()=>{
+        
+        work.forEach((work)=>
+        {
+            
+        
+            if(filter==='All'|| filter===work.attributes.target.value)
+            {
+                
+                
+                work.style.transition="all  1s ease ";
+                work.style.opacity=1;
+                work.style.transform= "translateY(0px)";
+                
+    
+            }     
+       
+        })
+        
+    },500)
+    work.forEach((work)=>
+    {
+        
+    
+        if(filter==='All'|| filter===work.attributes.target.value)
+        {
+            work.style.display="flex";
+           
+            
+            
+            
+            
 
+        }     
 
+    })},501)
+    setTimeout(()=>{
+        
+        work.forEach((work)=>
+        {   
+                
+                work.style.display="none";
+   
+        })
+        
+    },500)
+    work.forEach((work)=>
+    {
+        
+            
+            work.style.transition="all  1s ease";
+            work.style.opacity=0;
+            work.style.transform="translateY(100px)";
+    })
+} 
 
+//프로젝트 버튼 갯수 수정
+const probtncoutn=()=>{
+    var countA=0;
+    var countH=0;
+    var countC=0;
+    var countJ=0;
+    
+    work.forEach((work)=> {
+        if(work.attributes.target.value)
+        {
+            countA++;
+            
+
+        }
+        if('HTML'===work.attributes.target.value)
+        {
+            countH++;
+            
+        }
+            
+        else if('CSS'===work.attributes.target.value)
+        {
+            countC++;
+            
+        }   
+        else if('JS'===work.attributes.target.value)
+        {
+            countJ++;
+            
+        }
+        
+        
+    })
+    workbtn.children[1].children[0].innerText=countH;
+    workbtn.children[2].children[0].innerText=countC;
+    workbtn.children[3].children[0].innerText=countJ;
+    workbtn.children[0].children[0].innerText=countA;
+
+};
 
 
 
@@ -55,6 +156,8 @@ window.addEventListener('scroll',fixnav);
 menu.addEventListener('click',scrollmenu);
 contact.addEventListener('click',scrollmenu);
 window.addEventListener('scroll',opa);
-window.addEventListener('scroll',arrvis)
-arrow.addEventListener('click',uptotop)
-
+window.addEventListener('scroll',arrvis);
+arrow.addEventListener('click',uptotop);
+workbtn.addEventListener('click',proofilter);
+probtncoutn();
+console.log(workbtn.children[3].children[0]);
