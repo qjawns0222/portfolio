3d4cb543f5147cad1417063622fb27c706fe900d
qjawns0222 <33923212+qjawns0222@users.noreply.github.com>
2022-01-03 16:13:58 +0900

    js intersction obsrver

--- main.js
+++ main.js
@@ -10,7 +10,11 @@ const workbtn=document.querySelector(".work__catergories");
 const section=document.querySelectorAll("section");
 const ham=document.querySelector(".navbar__toggle>i")
 const navtop=nav.getBoundingClientRect().height;
-let check=0;
+var countA=0;
+var countH=0;
+var countC=0;
+var countJ=0;
+let check=1;
 //바에 클래스 추가해서 고정되게하는것(기본을 고정으로 하고 기본색깔을 transparent로 통해 투명하게 그리고 내려오면 색깔을 넣어서 만든다)
 const fixnav=()=>{
     if(window.scrollY>=nav.offsetHeight)
@@ -26,7 +30,7 @@ const scrollmenu=(event)=>{
     const locate=event.target.dataset.link;
     const skill=document.querySelector(locate);
     if(locate)
-        window.scrollTo( {top:skill.offsetTop-71, left:0, behavior: 'smooth',});
+        window.scrollTo( {top:skill.offsetTop-60, left:0, behavior: 'smooth',});
 }
 //home 투명도 스크롤 내릴때
 const opa=()=>home.style.opacity=1-window.scrollY/(home.offsetHeight-71);
@@ -47,14 +51,16 @@ const arrvis=()=>{
         
     }}
 
-// 제위로 당기는거
+// 젤위로 당기는거
 const uptotop=()=>window.scrollTo(0,0);
 //target 이름으로 프로젝트 숨기고 보이게 하는것 그리고 내려갔다 올라오는 애니매이션+ 다사라졌다가 다시 올라옴
 const proofilter=(event)=>{
     
     const filter=event.target.innerText.split(' ')[0];
-    if(event.target.classList.contains('active'))
+    
+    if(event.target.classList.contains('active') || event.target.classList.contains('work__catergories'))
         return;  
+   
     setTimeout(()=>
     {
         setTimeout(()=>
@@ -89,10 +95,7 @@ const proofilter=(event)=>{
 
 //프로젝트 버튼 갯수 수정
 const probtncoutn=()=>{
-    var countA=0;
-    var countH=0;
-    var countC=0;
-    var countJ=0;
+   
     
     work.forEach((work)=> 
     {
@@ -113,6 +116,8 @@ const probtncoutn=()=>{
 };
 //project active
 const worbtnactive=(event)=>{
+    if(event.target.classList.contains('active') || event.target.classList.contains('work__catergories'))
+        return; 
     const child=[...workbtn.children];
     child.forEach(btn=>
             {
@@ -121,12 +126,21 @@ const worbtnactive=(event)=>{
         )
     event.target.classList.add('active')  
 }
-//nav menu active
+//nav menu active //intersctionObserver 이거때문에 setTimeout 추가
 const navbtnactive=(event)=>
 {
     const child=[...menu.children];
-    child.forEach(btn=>btn.classList.remove('active'))
-    event.target.classList.add('active')   
+    setTimeout(()=>
+    {
+        child.forEach(btn=>btn.classList.remove('active'))
+        event.target.classList.add('active') 
+        if(window.innerWidth<=768) 
+        {
+            menus.style.display="none";
+            nav.classList.add("toggle")
+        }
+        check=1 
+    },10)
 }
 //scroll nav menu active //a<b<c는 무조건 true가 나온다
 const scrnavactive=()=>{
@@ -148,18 +162,53 @@ const scrnavactive=()=>{
 }
 //navbar 토글 버튼 
 const menusee=()=>{
-    ++check
+    
     if(check%2)
     {
         menus.style.display="flex";
-        nav.style.background="linear-gradient(45deg,#E16A96,#6775C9)";
+        nav.classList.add("toggle")
+        check=2
     }
     else
     {
         menus.style.display="none";
-        nav.style.background="transparent"
+        nav.classList.add("toggle")
+        check=1
     }
 }
+//intersctionObserver 스크롤시 navmenu해당 키 active
+let option={
+    root:null,
+    rootMargin:'0% 0% 0% 0%',
+    threshold:1,
+}
+const callback=(entries,observer)=>{
+    console.log(entries[0].isIntersecting,entries[0].boundingClientRect.y);
+    if(entries[0].isIntersecting!==true)
+    {
+        [...menu.children].forEach(child=>child.classList.remove('active'))
+        if(entries[0].boundingClientRect.y<0)
+        {
+            for(let i=0;i<[...menu.children].length;i++)
+            { 
+                if([...menu.children][i].dataset.link==='#'+entries[0].target.id)
+                    [...menu.children][i+1].classList.add('active')              
+            }
+        }
+    
+        else if(entries[0].boundingClientRect.y>0)
+        {
+            for(let i=0;i<[...menu.children].length;i++)
+            {
+                
+                if([...menu.children][i].dataset.link==='#'+entries[0].target.id)
+                        [...menu.children][i-1].classList.add('active')  
+            }
+        }
+    }   
+}
+let observer= new IntersectionObserver(callback,option);
+section.forEach(section=>{observer.observe(section)})
 window.addEventListener('scroll',fixnav);
 menu.addEventListener('click',scrollmenu);
 contact.addEventListener('click',scrollmenu);
@@ -170,5 +219,7 @@ workbtn.addEventListener('click',proofilter);
 probtncoutn();
 workbtn.addEventListener('click',worbtnactive);
 menu.addEventListener('click',navbtnactive);
-window.addEventListener('scroll',scrnavactive);
+//window.addEventListener('scroll',scrnavactive);
 ham.addEventListener('click',menusee);
+
+console.log([...menu.children][1].classList)
